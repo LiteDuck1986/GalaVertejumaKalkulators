f35fe086ca7baeca7f24a0831a12529e7b82f454
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-10-07 08:39:25 +0300

    Programma tika sadalīta pa metodem un tika izveidota izvelne.

--- src/pakotne/GalvenaKlase.java
+++ src/pakotne/GalvenaKlase.java
@@ -3,35 +3,145 @@ package pakotne;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class GalvenaKlase {
 	
 	static int studSk, kritSk;
 	static Scanner scan = new Scanner(System.in);
 	static DecimalFormat df = new DecimalFormat("0.#");
-	static String[] studenti = new String[studSk];
-	static String[] kriteriji = new String[kritSk];
-	static int[] kriterijaSvars = new int[kritSk];
-	static int[][] kriterijaVertejums = new int[studSk][kritSk];
-	static double[] semestraVertejums = new double[studSk];
+	static String[] studenti;
+	static String[] kriteriji;
+	static int[] kriterijaSvars;
+	static int[][] kriterijaVertejums;
+	static double[] semestraVertejums;
+	
+	String teksts = "";
 	
 	public static void main(String[] args) {
 		
+		String izvele;
+		String[] darbibuSaraksts = { "Ievadīt audzēkni", "Ievadīt kritērijus","Ievadīt kritērijas svaru", "Ievadīt vērtējumus", "Labot Kritērijus",
+				"Labot kritērijas svaru", "Labot iegūto vērtējumu", "Aprēķināt gala vērtējumu", "Saglabāt failā", "Apskatīt failu",
+				"Apturēt" };
+
+		do {
+			izvele = (String) JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību izvēle",
+					JOptionPane.QUESTION_MESSAGE, null, darbibuSaraksts, darbibuSaraksts[0]);
+
+			if (izvele == null)
+				izvele = "Apturēt";
+
+			switch (izvele) {
+			case "Ievadīt audzēkni":
+				IevaditAudz();
+				break;
+				
+			case "Ievadīt kritērijus":
+				IevaditKrit();
+				break;
+				
+			case "Ievadīt kritērijas svaru":
+				IevaditKritSvaru();
+				break;
+				
+			case "Ievadīt vērtējumus":
+				IevadiVertejumi();
+				break;
+				
+			case "Labot Kritērijus":
+//				to do;
+				break;
+				
+			case "Labot Kritērijas svaru":
+//				to do;
+				break;
+				
+			case "Labot iegūto vērtējumu":
+//				to do;
+				break;
+				
+			case "Aprēķināt gala vērtējumu":
+				AprekinatGalaVert();
+				break;
+
+			case "Saglabāt failā":
+
+				break;
+
+			case "Apskatīt failu":
+
+				break;
+
+			case "Apturēt":
+				JOptionPane.showMessageDialog(null, "Programma apturēta!", "Paziņojums",
+						JOptionPane.INFORMATION_MESSAGE);
+
+				break;
+			}
+
+		} while (!izvele.equals("Apturēt"));
+		scan.close();
+	}
 		
-		Metode1();
+	static void IevaditAudz() {
+		// Audzēkņu skaita ievade
+		do {
+			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+			while(!scan.hasNextInt()) {
+				System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+				scan.next();
+			}
+			studSk = scan.nextInt();
+		}while(studSk<1);
 		
-		System.out.println("Auf wiedersehen!");
-		System.out.println("Padevos.");
+		
+		studenti = new String[studSk];
 		
 		
-		// Definē kritērijus
-		int maxSvars = 100, sk = 1;
-		double atlSvars;
+		scan.nextLine();
+		
+		// Ievada audzēkņu vārdus, uzvārdus
+		for(int i=0; i<studenti.length; i++) {
+			do {
+				System.out.println("Ievadi "+(i+1)+". studentu");
+				studenti[i] = scan.nextLine().trim();
+			} while(!studenti[i].matches("^[\\p{L} ]+$"));
+		}
+		}
+	
+	public static void IevaditKrit() {
+		
+		// Vērtēšanas kritēriju skaita ievade
+				do {
+					System.out.println("Kāds būs kritēriju skaits?");
+					while(!scan.hasNextInt()) {
+						System.out.println("Kāds būs kritēriju skaits?");
+						scan.next();
+					}
+					kritSk = scan.nextInt();
+				}while(kritSk<1);
+				
+				kriteriji = new String[kritSk];
+		
 		for(int i=0; i<kriteriji.length; i++) {
 			do {
 				System.out.println("Ievadi "+(i+1)+". kritēriju");
 				kriteriji[i] = scan.nextLine().trim();
 			} while(!kriteriji[i].matches("^[\\p{L} ]+$"));
 			
+		}
+	}
+			
+		public static void IevaditKritSvaru() {
+			
+			// Definē kritērijus
+			int maxSvars = 100, sk = 1;
+			double atlSvars;
+			
+			kriterijaSvars = new int[kritSk];
+			
+			for(int i=0; i<kriterijaSvars.length; i++) {
 			// Norāda katra kritērija svaru
 			do {
 				System.out.println("Ievadi "+(i+1)+". kritērija svaru (max: "+maxSvars+")");
@@ -52,7 +162,12 @@ public class GalvenaKlase {
 			sk++;
 			scan.nextLine();
 		}
+		}
 		
+		
+		public static void IevadiVertejumi() {
+			
+			kriterijaVertejums = new int[studSk][kritSk];
 		// Norāda vērtējumu kādu ieguvis katrs audzēknis par katru kritēriju
 		for(int i=0; i<kriterijaVertejums.length; i++) {
 			for(int j=0; j<kriterijaVertejums[i].length; j++) {
@@ -66,7 +181,12 @@ public class GalvenaKlase {
 				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
 			}
 		}
+		}
 		
+		public static void AprekinatGalaVert() {
+		
+			semestraVertejums = new double[studSk];
+			
 		// Gala vērtējuma aprēķināšana
 		double rezultats;
 		for(int i=0; i<studenti.length; i++) {
@@ -75,6 +195,7 @@ public class GalvenaKlase {
 				rezultats += ((double) kriterijaSvars[j]/100)*kriterijaVertejums[i][j];
 			}
 			semestraVertejums[i] = rezultats;
+			
 		}
 		
 		// Gala vērtējumu izvadīšana
@@ -85,41 +206,7 @@ public class GalvenaKlase {
 			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+" balles"
 					+ "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 		}
-		scan.close();
+		
 	}
-	
-	static void Metode1() {
-		// Audzēkņu skaita ievade
-		do {
-			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
-			while(!scan.hasNextInt()) {
-				System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
-				scan.next();
-			}
-			studSk = scan.nextInt();
-		}while(studSk<1);
-		
-		
-		// Vērtēšanas kritēriju skaita ievade
-		do {
-			System.out.println("Kāds būs kritēriju skaits?");
-			while(!scan.hasNextInt()) {
-				System.out.println("Kāds būs kritēriju skaits?");
-				scan.next();
-			}
-			kritSk = scan.nextInt();
-		}while(kritSk<1);
-		
-		
-		scan.nextLine();
-		
-		// Ievada audzēkņu vārdus, uzvārdus
-		for(int i=0; i<studenti.length; i++) {
-			do {
-				System.out.println("Ievadi "+(i+1)+". studentu");
-				studenti[i] = scan.nextLine().trim();
-			} while(!studenti[i].matches("^[\\p{L} ]+$"));
-		}
-		}
 	
 }
